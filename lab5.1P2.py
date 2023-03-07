db1d6f2c0c27740af25dc432f7c53859dee2e399
Thomas Deboel <r0841840@student.thomasmore.be>
2023-03-07 11:20:29 +0100

    lab5.1 p2

---
+++ lab5.1P2.py
@@ -0,0 +1,30 @@
+import time
+import wiringpi
+import sys
+    
+
+print("start")
+pin=2
+pin_2=3
+pin_3=4
+pin_4=6
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(pin,1)
+wiringpi.pinMode(pin_2,1)
+wiringpi.pinMode(pin_3,1)
+wiringpi.pinMode(pin_4,1)
+i=0
+
+while i>=0:
+    i+=1
+    wiringpi.digitalWrite(pin, 1)
+    wiringpi.digitalWrite(pin_2, 1)
+    wiringpi.digitalWrite(pin_3, 1)
+    wiringpi.digitalWrite(pin_4, 1)
+    time.sleep(.1)
+    wiringpi.digitalWrite(pin, 0)
+    wiringpi.digitalWrite(pin_2, 0)
+    wiringpi.digitalWrite(pin_3, 0)
+    wiringpi.digitalWrite(pin_4, 0)
+    time.sleep(.1)
+    print(i)
